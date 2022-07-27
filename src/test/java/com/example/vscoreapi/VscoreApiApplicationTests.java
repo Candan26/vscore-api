20c37d1970418bdf3f152e9203eba2e596afe748
cagri <cagricandan91@gmail.com>
2022-07-27 22:04:09 +0300

    Initial commit

---
+++ src/test/java/com/example/vscoreapi/VscoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vscoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
